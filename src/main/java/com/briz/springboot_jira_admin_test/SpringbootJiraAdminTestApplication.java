e9b898a406fa328a1640b54772a61b61a338fa6c
kanchan raju <kanchanraju2015@gmail.com>
2025-09-18 13:18:18 +0530

    first admin commit

---
+++ src/main/java/com/briz/springboot_jira_admin_test/SpringbootJiraAdminTestApplication.java
@@ -0,0 +1,13 @@
+package com.briz.springboot_jira_admin_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJiraAdminTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJiraAdminTestApplication.class, args);
+	}
+
+}
