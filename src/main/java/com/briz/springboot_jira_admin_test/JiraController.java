88273e7c36370c7f195e36d22c917150dfe9f952
Anurag-Kumar-Poddar <anuragkumarpoddar736@gmail.com>
2025-09-19 12:47:22 +0530

    new endpoint added

--- src/main/java/com/briz/springboot_jira_admin_test/JiraController.java
+++ src/main/java/com/briz/springboot_jira_admin_test/JiraController.java
@@ -1,6 +1,8 @@
 package com.briz.springboot_jira_admin_test;
 
 
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +11,11 @@ public class JiraController {
     @RequestMapping("/test")
     public String test() {
         return "This is jira admin test";
+    }
+
+    @NotBlank(message = "The name cannot be blank")
+    @RequestMapping("/userInput")
+    public String userInput(@PathVariable String name) {
+        return "Hello " + name;
     }
 }
